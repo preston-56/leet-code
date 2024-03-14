7cb2c804e85d7cf89fe71e77bd70ad59e7d2007c
Preston Osoro <prestonosoro56@gmail.com>
2024-03-14 02:38:54 -0700

    solve twoSum leetcode question

--- twoSum.js
+++ twoSum.js
@@ -4,21 +4,21 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    const numMap = new Map();
+  const numMap = new Map();
 
-    for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (numMap.has(complement)) {
-            return [numMap.get(complement), i];
-        }
-        numMap.set(nums[i], i);
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numMap.has(complement)) {
+      return [numMap.get(complement), i];
     }
+    numMap.set(nums[i], i);
+  }
 
-    // If no solution found
-    return [];
+  // If no solution found
+  return [];
 };
 
 // Test cases
 console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
-console.log(twoSum([3, 2, 4], 6));       // Output: [1, 2]
-console.log(twoSum([3, 3], 6));          // Output: [0, 1]
+console.log(twoSum([3, 2, 4], 6)); // Output: [1, 2]
+console.log(twoSum([3, 3], 6)); // Output: [0, 1]
